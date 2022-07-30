54cdd3630a56e6a8a162e8cf04cda5ba3a2977e1
Kappy <edudas.me1997@gmail.com>
2022-07-30 17:32:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ sitemapper.add_endpoint("products_list.index", changefreq="daily", priority="1")
 def kappy_sitemap():
 	return sitemapper.generate()
 
-@payment.route('/webhook', methods=['POST', 'GET'])
+@app.route('/webhook', methods=['POST', 'GET'])
 @csrf.exempt
 def webhook():
      if request.method == 'POST':
