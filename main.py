d8fe5d22fc97b152c43dfca1b6931cf94dd26695
Kappy <edudas.me1997@gmail.com>
2022-08-04 03:26:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -54,8 +54,8 @@ def webhook():
           print(imd)
           print(type(imd))
           payment = SimpleNamespace(**imd)
-          print(payment.order_id)
-          update_pay_status("Pagamento confirmado", payment.order_id)
+          print(type(payment.order_id))
+          update_pay_status("Pagamento confirmado", payment.order_id[0])
           webhook_url = 'https://discord.com/api/webhooks/1001975186695925770/NDFvftZaOEL7FnbV_7q6oe1EuqtDrTyaGTIEwhcpOItRifOiCOv4lzp8QbegHz0ROAZW'
           data = { 'content': 'A payment has been completed!' }
           r = requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
